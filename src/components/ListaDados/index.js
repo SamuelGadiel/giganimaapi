e98a23925130ec49a3ae58e3a867d9c23b09b577
Samuel Gadiel <samuelgadiel@gmail.com>
2020-09-09 05:04:10 -0300

    Estilizando a NavLateral

---
+++ src/components/ListaDados/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListaDados(props) {
+  return (
+    <h3>
+      {props.dado}
+    </h3>
+  );
+}
+
+export default ListaDados;
--- src/components/NavLateral/index.js
+++ src/components/NavLateral/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Logo from '../../../src/assets/img/Logo.png';
 import "./NavLateral.css"
+import ListaDados from '../ListaDados';
+
+const listaDeDados = ["Home", "Lista Completa", "Atualizados", "Mais Assistidos", "Nova Temporada",
+  "Categorias", "Filtrar por Ano", "Favoritos"];
 
 function NavLateral() {
   return (
     <nav className="NavLateral">
-      <a href="/">
-        <img className="Logo" src={Logo} alt="GiganimaLogo" />
-      </a>
-      <a className="ButtonLink" href="/"></a>
+      <div className="Quadrado">
+
+        <a href="/">
+          <img className="Logo" src={Logo} alt="GiganimaLogo" />
+        </a>
+      </div>
+
+      {listaDeDados.map(dado =>
+        <ListaDados className="ListaDados" dado={dado} />
+      )}
+
     </nav>
   );
 }
