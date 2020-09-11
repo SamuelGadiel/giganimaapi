c407e1fe24df988b0e559e49e9d80c964e5c354e
Samuel Gadiel <samuelgadiel@gmail.com>
2020-09-11 10:49:20 -0300

    Adicionado icones com react-icons

--- src/components/ListaDados/index.js
+++ src/components/ListaDados/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { MdHome, MdUpdate, MdStar, MdLibraryBooks } from 'react-icons/md';
+import { FaListOl } from 'react-icons/fa';
+import { GoPlus } from 'react-icons/go';
+import { IoMdCalendar, IoMdHeart } from 'react-icons/io';
 
-function ListaDados(props) {
+const listaDeDados = ["Home", "Lista Completa", "Atualizados", "Mais Assistidos", "Nova Temporada",
+  "Categorias", "Filtrar por Ano", "Favoritos"];
+
+const icons = [<MdHome />, <FaListOl />, <MdUpdate />, <MdStar />, <GoPlus />, <MdLibraryBooks />, <IoMdCalendar />, <IoMdHeart />]
+
+function ListaDados() {
   return (
     <a href="/">
-      <h3>{props.dado}</h3>
+      {listaDeDados.map((texto, idTexto) => {
+        return (
+          <span key={`texto_${idTexto}`} className="ListaDados">
+            <span key={`icon_${idTexto}`} className="icon">{icons[idTexto]}</span>
+            {texto}
+          </span>)
+      })}
     </a>
   );
 }
--- src/components/NavLateral/index.js
+++ src/components/NavLateral/index.js
@@ -3,22 +3,16 @@ import Logo from '../../../src/assets/img/Logo.png';
 import "./NavLateral.css"
 import ListaDados from '../ListaDados';
 
-const listaDeDados = ["Home", "Lista Completa", "Atualizados", "Mais Assistidos", "Nova Temporada",
-  "Categorias", "Filtrar por Ano", "Favoritos"];
-
 function NavLateral() {
   return (
     <nav className="NavLateral">
       <div className="Quadrado">
-
         <a href="/">
           <img className="Logo" src={Logo} alt="GiganimaLogo" />
         </a>
       </div>
 
-      {listaDeDados.map(dado =>
-        <ListaDados className="ListaDados" dado={dado} />
-      )}
+      <ListaDados className="ListaDados" />
 
     </nav>
   );
