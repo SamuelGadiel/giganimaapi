292b46de722c7805324db8b78b76d1a1c8b5885d
Samuel Gadiel <samuelgadiel@gmail.com>
2020-09-12 02:14:36 -0300

    Inicio da barra de busca

--- src/components/BarraBusca/index.js
+++ src/components/BarraBusca/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './BarraBusca.css'
+import { MdSearch } from 'react-icons/md';
 
 function BarraBusca() {
   return (
-    <div></div>
+    <div className="BarraBusca">
+      <div className="campoBusca">
+        <MdSearch className="iconeBusca" />
+        <span className="textoBusca">Buscar</span>
+      </div>
+    </div>
   );
 }
 
